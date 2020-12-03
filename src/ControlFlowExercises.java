a9105f5e7000f5c6ebe42f7c24bc0031348de7e8
Eleanor Ellingson <eleanor.ellingson@gmail.com>
2020-12-03 15:18:23 -0600

    Do While squared

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -17,23 +17,26 @@ public class ControlFlowExercises {
 //        }
 //        while (i <= 100);
 
-//        int i = 105;
+//        int i = 100;
 //
 //        do {
-//            System.out.println(i -= 5);
-//
+//            System.out.println(i);
+//            i -= 5;
 //
 //        }
-//        while (i >= -5);
+//        while (i >= -10);
 
-        int i = 105;
+        int i = 2;
 
         do {
-            System.out.println(i -= 5);
-
+            System.out.println(i);
+           i = (int) Math.pow(i, 2);
 
         }
-        while (i >= -5);
+        while (i < 1000000);
+
+
+
 
 
     }
