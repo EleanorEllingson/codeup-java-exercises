722cccab646d672ce1834eb384d3fbadd03c33e0
Eleanor Ellingson <eleanor.ellingson@gmail.com>
2020-12-03 14:14:24 -0600

    Do While 2's

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,14 +1,21 @@
 public class ControlFlowExercises {
     public static void main(String[] args){
 
-        int i = 5;
+//        int i = 5;
+//
+//        while (i <= 15) {
+//            System.out.println(i);
+//            i++;
+//        }
 
-        while (i <= 15) {
-            System.out.println(i);
+        int i = 1;
+
+        do {
+            System.out.println(i += 1);
+
             i++;
         }
-
-
+        while (i <= 100);
 
 
 
