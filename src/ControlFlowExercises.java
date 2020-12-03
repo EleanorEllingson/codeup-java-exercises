c74a08b65d09dda56f0b235dd7977f375f1b74e7
Eleanor Ellingson <eleanor.ellingson@gmail.com>
2020-12-03 16:03:33 -0600

    Fizz Buzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -26,14 +26,38 @@ public class ControlFlowExercises {
 //        }
 //        while (i >= -10);
 
-        int i = 2;
+//        int i = 2;
+//
+//        do {
+//            System.out.println(i);
+//           i = (int) Math.pow(i, 2);
+//
+//        }
+//        while (i < 1000000);
 
-        do {
-            System.out.println(i);
-           i = (int) Math.pow(i, 2);
 
+//        for(int i = 2; i < 1000000;){
+//            System.out.println(i);
+//            i = (int) Math.pow(i, 2);
+//        }
+
+
+        for(int i = 1; i <= 100; i++){
+
+            if (i % 3 == 0){
+                System.out.println("Fizz");
+            }
+            if (i % 5 == 0){
+                System.out.println("Buzz");
+            }
+            if (i % 15 == 0){
+                System.out.println("FizzBuzz");
+            }
+            else{
+                System.out.println(i);
+            }
         }
-        while (i < 1000000);
+
 
 
 
