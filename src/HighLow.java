ceaa85e7bb9760047530a657f17ab1a8ae192a68
Eleanor Ellingson <eleanor.ellingson@gmail.com>
2020-12-07 11:05:06 -0600

    Finished question 5

--- src/HighLow.java
+++ src/HighLow.java
@@ -6,15 +6,21 @@ public class HighLow {
         Random rand = new Random();
         int randNum = rand.nextInt(100);
         boolean stillGuessing = true;
+        int count = 0;
         while (stillGuessing) {
-            System.out.println("Guess a number between 1 and 100.");
+            System.out.println("Guess a number between 1 and 100. (you only have 6 guesses!)");
             Scanner userInput = new Scanner(System.in);
-            System.out.println("Random number: " + randNum);
+
 
             int enteredNum = userInput.nextInt();
             if (enteredNum >= 1 && enteredNum <= 100) {
                 System.out.println("Valid Entry");
-
+                    count ++;
+                    System.out.println("You have used: " + count + " guesses.");
+                    if(count > 5){
+                        System.out.println("You lose");
+                        stillGuessing = false;
+                    }
                 if (enteredNum == randNum) {
                     System.out.println("GOOD GUESS!");
                     stillGuessing = false;
