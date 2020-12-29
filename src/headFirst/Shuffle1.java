c179c8efc55a1867a605643026afa8252375ddef
Eleanor Ellingson <eleanor.ellingson@gmail.com>
2020-12-29 10:26:52 -0600

    Be the compiler

--- src/headFirst/Shuffle1.java
+++ src/headFirst/Shuffle1.java
@@ -19,6 +19,31 @@ public class Shuffle1 {
             }
 
         }
+        System.out.println("");
+        int w = 5;
+        while (w > 1){
+            w = w - 1;
+            if (w < 3) {
+                System.out.println("small x");
+            }
+        }
+
+        int y = 1;
+        while (y < 10){
+            y = y + 1;
+            if (y > 3) {
+                System.out.println("big x");
+                y = y + 1;
+            }
+        }
+
+        int z = 5;
+        while (z > 1){
+            z = z - 1;
+            if (z < 3){
+                System.out.println("small x");
+            }
+        }
 
     }
 }
