3f019787c18d71df0eb07448b7409ff722d6ba88
Eleanor Ellingson <eleanor.ellingson@gmail.com>
2021-01-06 21:27:07 -0600

    Finished DVD Player compiler exercise

---
+++ src/headFirst/DVDPlayer.java
@@ -0,0 +1,15 @@
+package headFirst;
+
+public class DVDPlayer {
+    boolean canRecord = false;
+
+    void recordDVD(){
+
+        System.out.println("DVD recording");
+
+    }
+
+    void playDVD(){
+        System.out.println("DVD playing");
+    }
+}
---
+++ src/headFirst/DVDPlayerTestDrive.java
@@ -0,0 +1,15 @@
+package headFirst;
+
+public class DVDPlayerTestDrive {
+    public static void main (String[] args) {
+        DVDPlayer d = new DVDPlayer();
+        d.canRecord = true;
+        d.playDVD();
+
+        if(d.canRecord){
+            d.recordDVD();
+        }
+    }
+
+
+}
