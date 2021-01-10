14f33f85cc2de9fc952d9c0a3e9780c2e5cc1442
Eleanor Ellingson <eleanor.ellingson@gmail.com>
2021-01-09 20:22:29 -0600

    Practicing Scanner

---
+++ src/solutions/Solution.java
@@ -0,0 +1,33 @@
+package solutions;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+
+        System.out.println("Enter a number.");
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        System.out.println("Enter a number.");
+        int b = scan.nextInt();
+        System.out.println("Enter a number.");
+        int c = scan.nextInt();
+        // Complete this line
+
+
+        // Complete this line
+
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+        // Complete this line
+        // Complete this line
+    }
+}
+
