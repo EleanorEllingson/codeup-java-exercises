5b345cc68c6b2ab29259d37bcf7ed009d4309e8a
Eleanor Ellingson <eleanor.ellingson@gmail.com>
2021-01-10 00:30:29 -0600

    used String.format

--- src/solutions/Solution.java
+++ src/solutions/Solution.java
@@ -6,41 +6,51 @@ public class Solution {
 
     public static void main(String[] args) {
         /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
-        System.out.println("Hello, World.");
-        System.out.println("Hello, Java.");
-
-        System.out.println("Enter a number.");
-        Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        System.out.println("Enter a number.");
-        int b = scan.nextInt();
-        System.out.println("Enter a number.");
-        int c = scan.nextInt();
+//        System.out.println("Hello, World.");
+//        System.out.println("Hello, Java.");
+//
+//        System.out.println("Enter a number.");
+//        Scanner scan = new Scanner(System.in);
+//        int a = scan.nextInt();
+//        System.out.println("Enter a number.");
+//        int b = scan.nextInt();
+//        System.out.println("Enter a number.");
+//        int c = scan.nextInt();
         // Complete this line
 
 
         // Complete this line
 
 
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println(c);
 
         // Complete this line
         // Complete this line
 
-        System.out.println("Enter a number.");
-        int i = scan.nextInt();
-        String temp = scan.nextLine();
-        System.out.println("Enter a name.");
-        String s = scan.nextLine();
-        System.out.println("Enter a double.");
-        Double d = scan.nextDouble();
-        // Write your code here.
+//        System.out.println("Enter a number.");
+//        int i = scan.nextInt();
+//        String temp = scan.nextLine();
+//        System.out.println("Enter a name.");
+//        String s = scan.nextLine();
+//        System.out.println("Enter a double.");
+//        Double d = scan.nextDouble();
+//        // Write your code here.
+//
+//        System.out.println("String: " + s);
+//        System.out.println("Double: " + d);
+//        System.out.println("Int: " + i);
 
-        System.out.println("String: " + s);
-        System.out.println("Double: " + d);
-        System.out.println("Int: " + i);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("================================");
+
+            String s1 = "java";
+            int x = 12;
+            //Complete this line
+        System.out.println(String.format("%-15.15s", s1) + String.format("%03d", x));
+
+        System.out.println("================================");
 
     }
 }
