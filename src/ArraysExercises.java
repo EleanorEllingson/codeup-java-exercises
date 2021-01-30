467a2589ad8f7c769bb7ef9f916d9bb0e112ef32
Eleanor Ellingson <eleanor.ellingson@gmail.com>
2021-01-29 23:27:36 -0600

    added new array

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 public class ArraysExercises {
     public static Person[] addPerson(Person[] oldPersonArray) {
         Person[] newPersonArray = new Person[oldPersonArray.length + 1];
-        for(int i = 0; i < oldPersonArray.length; i++){
+        for (int i = 0; i < oldPersonArray.length; i++) {
             newPersonArray[i] = oldPersonArray[i];
         }
         return newPersonArray;
@@ -12,7 +12,7 @@ public class ArraysExercises {
     }
 
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5};
         System.out.println(Arrays.toString(numbers));
         Person[] personArray = new Person[3];
@@ -20,19 +20,22 @@ public class ArraysExercises {
         personArray[1] = new Person("Ethan");
         personArray[2] = new Person("Michael");
 //        System.out.println(Arrays.toString(personArray));
-        for(Person person : personArray){
+        for (Person person : personArray) {
             System.out.println(person.getName());
         }
         personArray = addPerson(personArray);
         personArray[3] = new Person("Rebecca");
-        for(Person person : personArray){
+        for (Person person : personArray) {
             System.out.println(person.getName());
         }
 
+        personArray = addPerson(personArray);
+        personArray[4] = new Person("Elena");
+        for (Person person : personArray) {
+            System.out.println(person.getName());
         }
 
-
-
     }
+}
 
 
