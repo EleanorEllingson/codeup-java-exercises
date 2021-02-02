930e157c6e6ad660c9ec0a783d3ffd3333368a36
Eleanor Ellingson <eleanor.ellingson@gmail.com>
2021-02-01 21:10:56 -0600

    Unique characters method and permutation check method

--- src/LeetCodePractice.java
+++ src/LeetCodePractice.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class LeetCodePractice {
     public static int rotatedDigits(Integer N) {
         int count = 0;
@@ -9,10 +11,37 @@ public class LeetCodePractice {
         return count;
     }
 
+   public static boolean uniqueCharacters(String str){
+        for(int i = 0; i < str.length(); i++){
+            for(int j = i + 1; j < str.length(); j++){
+                if (str.charAt(i) == str.charAt(j)) {
+                    return false;
+                }
+            }
+
+        }
+        return true;
+    }
+
+    public static boolean permutationCheck(String str1, String str2){
+        char tempArray[] = str1.toCharArray();
+        Arrays.sort(tempArray);
+        String string1 = new String(tempArray);
+        char tempArray2[] = str2.toCharArray();
+        Arrays.sort(tempArray2);
+        String string2 = new String(tempArray2);
+        if(string1.equals(string2)){
+            return true;
+        }
+        return false;
+    }
+
     public static void main(String[] args){
 
 
         System.out.println(rotatedDigits(20));
+        System.out.println(uniqueCharacters("eleanor"));
+        System.out.println(permutationCheck("dad", "ads"));
     }
 };
 
